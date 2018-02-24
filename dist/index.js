463101010e2eb4c84d9ad982383ad8c14c16b6fb
Matas Valužis <matas.valuzis@gmail.com>
2018-02-24 13:43:24 +0200

    Updated babel to transpile async to generators

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,8 @@ exports.Resolver = Resolver;
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var UnresolvedAction = function () {
@@ -38,11 +40,30 @@ var UnresolvedAction = function () {
         }
     }, {
         key: "execute",
-        value: async function execute(dependencies) {
-            var _resolver;
+        value: function () {
+            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(dependencies) {
+                var _resolver;
 
-            return (_resolver = this.resolver).call.apply(_resolver, [dependencies].concat(_toConsumableArray(this.args)));
-        }
+                return regeneratorRuntime.wrap(function _callee$(_context) {
+                    while (1) {
+                        switch (_context.prev = _context.next) {
+                            case 0:
+                                return _context.abrupt("return", (_resolver = this.resolver).call.apply(_resolver, [dependencies].concat(_toConsumableArray(this.args))));
+
+                            case 1:
+                            case "end":
+                                return _context.stop();
+                        }
+                    }
+                }, _callee, this);
+            }));
+
+            function execute(_x2) {
+                return _ref.apply(this, arguments);
+            }
+
+            return execute;
+        }()
     }]);
 
     return UnresolvedAction;
